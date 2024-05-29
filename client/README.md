abe150569b35257ee8d56ed55fe0156899b3806b
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-05-29 11:48:59 +0530

    env setup

---
+++ client/README.md
@@ -0,0 +1,2 @@
+# Client Folder
+This folder contain all the client side code for hello-world chat app
---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Button>Hello World</Button>
+    </div>
+  )
+}
